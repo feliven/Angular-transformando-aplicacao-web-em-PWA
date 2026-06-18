206e32750e8a643dc23c4317e22485b276875174
FVS-MBA <feliven@gmail.com>
2026-06-18 19:59:09 -0300

    prepara app para reproduzir sons

---
+++ src/app/shared/services/audio.service.ts
@@ -0,0 +1,56 @@
+import { Service } from '@angular/core';
+import { Howl } from 'howler';
+
+@Service()
+export class AudioService {
+  private audioTracks: Map<string, Howl> = new Map();
+
+  constructor() {
+    this.initializeTracks();
+  }
+
+  private initializeTracks() {
+    this.audioTracks.set(
+      'environment',
+      new Howl({
+        src: ['./assets/sounds/luna-rise-part-one.mp3'],
+      }),
+    );
+
+    this.audioTracks.set(
+      'play',
+      new Howl({
+        src: ['./assets/sounds/play.wav'],
+      }),
+    );
+
+    this.audioTracks.set(
+      'pause',
+      new Howl({
+        src: ['./assets/sounds/pause.mp3'],
+      }),
+    );
+
+    this.audioTracks.set(
+      'beep',
+      new Howl({
+        src: ['./assets/sounds/beep.mp3'],
+      }),
+    );
+  }
+
+  play(trackName: string) {
+    const track = this.audioTracks.get(trackName);
+    track?.play();
+  }
+
+  pause(trackName: string) {
+    const track = this.audioTracks.get(trackName);
+    track?.pause();
+  }
+
+  stop(trackName: string) {
+    const track = this.audioTracks.get(trackName);
+    track?.stop();
+  }
+}
